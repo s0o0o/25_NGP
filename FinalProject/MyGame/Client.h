972aba04998929800eff67bb8edf0fe412fe786a
xaeux0107 <ekdms1324@gmail.com>
2025-10-31 14:54:31 +0900

    클라이언트 서버 접속 코드 추가
    소켓 생성 및 서버 연결 기능 추가

---
+++ FinalProject/MyGame/Client.h
@@ -0,0 +1,46 @@
+//===================================================
+// 클라이언트 추가 - 2025.10.31
+//===================================================
+#pragma once
+#include "Common.h"
+
+char* SERVERIP = (char*)"127.0.0.1";
+#define SERVERPORT 9000
+#define BUFSIZE    512
+
+SOCKET sock;
+
+void InitClient() // 클라이언트 초기화
+{
+	int retval;
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		printf("윈도우 소켓 생성 실패\n");
+		exit(0);
+	}
+
+	// 소켓 생성
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET) err_quit("socket()");
+
+	// connect()
+	struct sockaddr_in serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	inet_pton(AF_INET, SERVERIP, &serveraddr.sin_addr);
+	serveraddr.sin_port = htons(SERVERPORT);
+	retval = connect(sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) err_quit("connect()");
+}
+
+void CloseClient() // 클라이언트 종료
+{
+	// 소켓 닫기
+	closesocket(sock);
+
+	// 윈속 종료
+	WSACleanup();
+}
